7d325d8f370d5b5aa1759720c27a68b593ec984b
Simon Evans <srevans@me.com>
2019-10-09 17:29:49 +0100

    suitably parameterised cost functions?

--- test_power_function.r
+++ test_power_function.r
@@ -4,17 +4,31 @@ library("lattice")
 # relating flock size with 
 # survival cost of migration
 
-N <- seq(0,1000,1)
+N <- seq(0,5000,10)
 
-pow <- 1.2
+pow <- 3.2
 
 max_migration_cost <- 1.0
 migration_cost_decay <- 0
-migration_cost_nonlinear_decay <- 0.001
+migration_cost_nonlinear_decay <- 0.0000000000012
 
-power.shizzle <- max_migration_cost - migration_cost_decay * N - migration_cost_nonlinear_decay *   N^pow
+power.shizzle <- max_migration_cost - migration_cost_decay * N - migration_cost_nonlinear_decay * N^pow
 
 print(
         xyplot(power.shizzle ~ N
                 ,type="l"
                 ,ylim=c(0,1)))
+                
+
+pow <- 0.62
+
+max_migration_cost <- 1.0
+migration_cost_decay <- -0.0003
+migration_cost_nonlinear_decay <- 0.012
+
+power.shizzle <- max_migration_cost - migration_cost_decay * N - migration_cost_nonlinear_decay * N^pow
+
+print(
+        xyplot(power.shizzle ~ N
+                ,type="l"
+                ,ylim=c(0,1)))                
