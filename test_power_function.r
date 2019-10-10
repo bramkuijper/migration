0be09c30446d2b379ea3c3f8301b568f97d7b9f9
Simon Evans <srevans@me.com>
2019-10-10 16:01:24 +0100

    nothing really

--- test_power_function.r
+++ test_power_function.r
@@ -20,6 +20,20 @@ print(
                 ,ylim=c(0,1)))
                 
 
+pow <- 1
+
+max_migration_cost <- 1.0
+migration_cost_decay <- 0.0002
+migration_cost_nonlinear_decay <- 0
+
+power.shizzle <- max_migration_cost - migration_cost_decay * N - migration_cost_nonlinear_decay * N^pow
+
+print(
+        xyplot(power.shizzle ~ N
+                ,type="l"
+                ,ylim=c(0,1)))                  
+                
+
 pow <- 0.62
 
 max_migration_cost <- 1.0
