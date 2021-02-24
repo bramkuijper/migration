dea5588a7ad120c7f585dd57dc23169814eeb12e
Simon Evans <srevans@me.com>
2021-02-24 09:15:44 +0000

    And lastly removing the trial parameters (short runtime and small population). Will work with a population size of 1K (rather than 2K) to speed up the simulations.

--- migration.cpp
+++ migration.cpp
@@ -33,10 +33,10 @@ uniform_real_distribution<> uniform(0.0,1.0);
 // function
 
 // number of individuals in population
-const int N = 200;  // DEAFULT: 2000
+const int N = 1000;  // DEAFULT: 2000
 
 // number of generations
-long int number_generations = 100;  // DEFAULT: 1000000
+long int number_generations = 1000000;  // DEFAULT: 1000000
 
 // sampling interval
 //int skip = ceil(number_generations / 500);
