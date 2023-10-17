ddf50af11c86692ac37799d69234c1c0e21faee6
Simon Evans <srevans@me.com>
2023-10-17 10:38:19 +0100

    Trying to get around a git clash I inadvertently created.

--- migration.cpp
+++ migration.cpp
@@ -26,10 +26,10 @@ std::uniform_real_distribution<> uniform(0.0,1.0);
 // values of the most of these are overridden in the init_arguments() function
 
 // number of individuals in population
-const int N = 500;  // DEFAULT: 2000
+const int N = 1000;  // DEFAULT: 2000
 
 // number of generations
-long int number_generations = 50;  // DEFAULT: 1000000
+long int number_generations = 1000000;  // DEFAULT: 1000000
 
 // sampling interval
 int skip = std::ceil((double)number_generations / 500);
