350185fa73cecd62d012e72298c6bc8dfd968e9d
Simon Evans <srevans@me.com>
2025-06-12 10:28:11 +0100

    Had erroneously set the initial value for phi_b to 0, meaning that it was fixed at that value and unable to evolve. What a dummy.

--- generate_parameters.py
+++ generate_parameters.py
@@ -5,7 +5,7 @@ import datetime
 init_theta_a = [60]  # Default is 60
 init_theta_b = [1]  # Default is 1
 init_phi_a = [0.3]  # Default is 0.3
-init_phi_b = [0]  # Default is 25
+init_phi_b = [25]  # Default is 25
 init_psi_a = 60
 init_psi_b = 0
 
--- migration.cpp
+++ migration.cpp
@@ -1705,8 +1705,8 @@ void postbreeding_dynamics(int t)
     // actual autumn dispersal at time t
     for (int i = 0; i < staging_pop; ++i)
     {		
-		pdisperse = pow(1.0 + exp(-0.5 * (StagingPool[i].phi_b[0] + StagingPool[i].phi_b[1]) * (((double) staging_pop_start / (staging_pop_start + winter_pop)) 	- 0.5 * (StagingPool[i].phi_a[0] + StagingPool[i].phi_a[1]))
-								-0.5 * (StagingPool[i].psi_b[0] + StagingPool[i].psi_b[1]) * (StagingPool[i].resources - 0.5 * (StagingPool[i].psi_a[0] + StagingPool[i].psi_a[1]))
+		pdisperse = pow(1.0 + exp(-0.5 * (StagingPool[i].phi_b[0] + StagingPool[i].phi_b[1]) * (((double) staging_pop_start / (staging_pop_start + winter_pop)) - 0.5 * (StagingPool[i].phi_a[0] + StagingPool[i].phi_a[1]))
+								  -0.5 * (StagingPool[i].psi_b[0] + StagingPool[i].psi_b[1]) * (StagingPool[i].resources - 0.5 * (StagingPool[i].psi_a[0] + StagingPool[i].psi_a[1]))
 							),
 			 		-1.0);
 
